9c9b6e764ac26fcabea667d71dd2d3c7dfd92783
Michi Tanaka <tanaka.michi@icloud.com>
2024-08-12 02:50:20 -0700

    Fixing minor error

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 // import logo from './logo.svg';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './HomePage';
-import About from './About';
+import HomePage from './HomePage.js';
+import About from './About.js';
 import './App.css';
 
 function App() {
